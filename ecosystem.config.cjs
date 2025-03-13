13eb5bc1b6c41b1577823c18857945514e3f06f7
arief sangga <arif.sangga@gmail.com>
2025-03-13 09:15:39 +0700

    update dist

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       args: "-s build",
       interpreter: "none",
       env: {
-        PM2_SERVE_PATH: "./build",
+        PM2_SERVE_PATH: "./dist",
         PM2_SERVE_PORT: 5000,
         PM2_SERVE_SPA: "true",
       },
