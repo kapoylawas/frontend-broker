9adae7c3d23e78037441bc25f1ff400c15492265
arief sangga <arif.sangga@gmail.com>
2025-03-13 09:07:55 +0700

    updte file cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "REACT-PREVIEW",
+      script: "serve",
+      args: "-s build",
+      interpreter: "none",
+      env: {
+        PM2_SERVE_PATH: "./build",
+        PM2_SERVE_PORT: 5000,
+        PM2_SERVE_SPA: "true",
+      },
+    },
+  ],
+};
