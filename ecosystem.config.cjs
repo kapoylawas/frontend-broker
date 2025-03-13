1c23f7c89ee470d28e5803438f16a8cd72d2711e
arief sangga <arif.sangga@gmail.com>
2025-03-13 21:58:53 +0700

    update comen cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,15 @@
-module.exports = {
-  apps: [
-    {
-      name: "REACT-PREVIEW",
-      script: "serve",
-      args: "-s build",
-      interpreter: "none",
-      env: {
-        PM2_SERVE_PATH: "./dist",
-        PM2_SERVE_PORT: 5000,
-        PM2_SERVE_SPA: "true",
-      },
-    },
-  ],
-};
+// module.exports = {
+//   apps: [
+//     {
+//       name: "REACT-PREVIEW",
+//       script: "serve",
+//       args: "-s build",
+//       interpreter: "none",
+//       env: {
+//         PM2_SERVE_PATH: "./dist",
+//         PM2_SERVE_PORT: 5000,
+//         PM2_SERVE_SPA: "true",
+//       },
+//     },
+//   ],
+// };
